0d71f213175861a4679698380a62eb7c8aaf291c
Gabriel <gabrielbtegani@gmail.com>
2024-06-03 15:05:48 -0300

    chore: Update select element cursor style

--- src/pages/components/Select.tsx
+++ src/pages/components/Select.tsx
@@ -8,13 +8,13 @@ export const Select = ({locale, changeOption}:SelectProps) => {
       <select 
          onChange={changeOption}
          className='bg-input-container border-2 border-solid border-border 
-         rounded-lg h-10 cursor-pointer w-20 sm:w-24'
+         rounded-lg h-10 cursor-pointer w-24'
       >
          <option value="allTasks">All</option>
          {locale === 'task' ? ( 
             <>
                <option value="done">Done</option>
-               <option value="toDo">Tasks</option>
+               <option value="toDo">Task</option>
             </>
          ) : (
             <>
